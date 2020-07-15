60132ca26b0bd003d61f730d1ec81731de804331
Ashika Shallow <slwashika@gmail.com>
2020-07-15 11:17:37 -0400

    Code to separate the words

---
+++ src/wordseparator/WordSeparator.java
@@ -0,0 +1,41 @@
+
+package wordseparator;
+
+/**
+ * This program separates run together strings
+ * @author Ashika Shallow July 14, 2020
+ */
+
+import java.util.Scanner;
+
+public class WordSeparator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner kb = new Scanner(System.in);
+        String input;
+              
+        System.out.println("Enter a run sentence: ");
+        input = kb.nextLine();
+        
+        System.out.println();
+        
+        StringBuilder str = new StringBuilder();       
+        char[] inputArray = input.toCharArray();
+                      
+        // Insert spaces at uppercase letters
+        for(int i = 0; i < inputArray.length; i++){           
+            str.append(inputArray[i]);
+            if(Character.isUpperCase(inputArray[i]) && i != 0)
+                str.insert(str.length() - 1, " ");
+            
+        }
+        
+        System.out.println(str);
+            
+    }
+    
+}
